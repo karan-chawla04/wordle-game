e22cd185c192fee846df8a54efe8da7dad305f53
karan-chawla04 <karan.chawla.04.11.98@gmail.com>
2025-01-17 09:44:56 +0530

    Changes: added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./style.css";
+import { WordleIcon } from "../../images";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="headerContainer">
+      <div onClick={navigateHome} className="headerLogo">
+        <div className="headerIcon">
+          <img src={WordleIcon} alt="" />
+        </div>
+        <h1>Wordle</h1>
+      </div>
+      <div className="headerContent">
+        <div>Some</div>
+        <div>Some</div>
+        <div>Some</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 
 const Game = () => {
-  return <div>Game</div>;
+  return <Header />;
 };
 
 export default Game;
